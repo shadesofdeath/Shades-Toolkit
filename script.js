616d69ea87224ffac31357506dd20170c0205311
Berkay AY <61064047+shadesofdeath@users.noreply.github.com>
2023-05-01 01:10:24 +0300

    :+1:

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
-function openPopup() {
-  document.getElementById("popup").style.display = "block";
+window.onload = function() {
+  document.getElementById("changelog-popup").style.display = "none";
+  document.getElementById("about-popup").style.display = "none";
 }
 
-function closePopup() {
-  document.getElementById("popup").style.display = "none";
+function openChangelogPopup() {
+  document.getElementById("changelog-popup").style.display = "block";
+}
+
+function closeChangelogPopup() {
+  document.getElementById("changelog-popup").style.display = "none";
+}
+
+function openAboutPopup() {
+  document.getElementById("about-popup").style.display = "block";
+}
+
+function closeAboutPopup() {
+  document.getElementById("about-popup").style.display = "none";
 }
